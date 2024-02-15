a6eba38ab9771d253f41b3e80781afaec3f7b2f7
Ramon Xavier <ramon1234rxm@gmail.com>
2024-02-15 20:08:57 -0300

    chore: adding vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
+import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   integrations: [react(), tailwind({ applyBaseStyles: false })],
   output: "server",
+  adapter: vercel(),
 });
