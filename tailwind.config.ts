af8346a5c97074e482b55df1ddfbcc8c052ee98f
Ramon Xavier <ramon1234rxm@gmail.com>
2024-02-28 20:32:22 -0300

    chore: again return to use next.js

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,13 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+import type { Config } from "tailwindcss";
+
+const config = {
   darkMode: ["class"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   prefix: "",
   theme: {
     container: {
@@ -53,16 +59,6 @@ export default {
         sm: "calc(var(--radius) - 4px)",
       },
       keyframes: {
-        text: {
-          "0%, 100%": {
-            "background-size": "200% 200%",
-            "background-position": "left center",
-          },
-          "50%": {
-            "background-size": "200% 200%",
-            "background-position": "right center",
-          },
-        },
         "accordion-down": {
           from: { height: "0" },
           to: { height: "var(--radix-accordion-content-height)" },
@@ -73,11 +69,12 @@ export default {
         },
       },
       animation: {
-        text: "text 5s ease infinite",
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
   plugins: [require("tailwindcss-animate")],
-};
+} satisfies Config;
+
+export default config;
