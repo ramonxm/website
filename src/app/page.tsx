6457fe4ac816e12af7465973b5ddd1c21562d135
Ramon Xavier <ramon1234rxm@gmail.com>
2024-03-05 12:50:44 -0300

    feat: adding background on home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
-import { Cards } from "@/components/modules/Cards";
+import { Background } from "@/components/modules/Background";
 import { Title } from "@/components/modules/Title";
 import { TextGenerateEffect } from "@/components/ui/text-generate-effect";
 
 export default function Home() {
   return (
-    <div className="mx-auto max-w-5xl flex-col items-center justify-center dark:bg-background mt-8">
-      <Title>Software Engineer</Title>
-      <TextGenerateEffect words="Full Stack Developer, based on React, React Native, Typescript and Node. I'm passionate about the entire programming ecosystem." />
+    <div className="h-screen">
+      <section className="dark:bg-background relative overflow-hidden h-4/6	items-center flex mx-auto max-w-5xl">
+        <Background />
+        <div className="flex-col items-center justify-center">
+          <Title>Software Engineer</Title>
+          <TextGenerateEffect words="Full Stack Developer, based on React, React Native, Typescript and Node. I'm passionate about the entire programming ecosystem." />
+        </div>
+      </section>
     </div>
   );
 }
--- src/components/modules/Header.tsx
+++ src/components/modules/Header.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export const Header = () => {
   return (
-    <header className="mx-auto flex max-w-5xl items-center justify-between gap-20 px-4 py-4 xl:px-0 flex-row lg:flex-row">
+    <header className="mx-auto flex max-w-5xl items-center justify-between gap-20 px-4 py-6 xl:px-0 flex-row lg:flex-row">
       <Link href="/">
         <Image src="/images/logo_name.png" alt="Logo" width={160} height={34} />
       </Link>
