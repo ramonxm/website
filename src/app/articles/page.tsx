c51563cb74d0441684b0864a42a113f4f95749e8
Ramon Xavier <ramon1234rxm@gmail.com>
2024-03-18 23:02:42 -0300

    chore: updating deeps, add eslint plugin import and update articles

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,4 +1,3 @@
-import { Layout } from "@/components/layouts/Layout";
 import { Cards } from "@/components/modules/Cards";
 import { Title } from "@/components/modules/Title";
 import { getAllArticles } from "@/lib/article";
@@ -10,6 +9,7 @@ export default async function Articles() {
     <div className="mx-auto max-w-5xl flex-col items-center justify-center dark:bg-background mt-8">
       <Title>Articles</Title>
       <div className="flex flex-col mt-6 gap-6">
+        <p>My articles published in Portuguese and English. 🇧🇷🇺🇸</p>
         <Cards items={articles} />
       </div>
     </div>
