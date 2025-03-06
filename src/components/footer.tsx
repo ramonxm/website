221f3065d4d367f06df02ae3afa90ec785edd8ff
Ramon Xavier <ramon1234rxm@gmail.com>
2025-03-06 20:24:29 -0300

    feat(mailto): send email to me

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,20 +1,12 @@
 "use client";
 
-import { validateEmail } from "#/utils/valide-email";
 import { ArrowUpRight } from "lucide-react";
-import { useState, FormEventHandler } from "react";
+import { FormEventHandler } from "react";
 
 export const Footer = () => {
-  const [email, setEmail] = useState("");
-  const [isEmailValid, setIsEmailValid] = useState(true);
   const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
-
-    if (validateEmail(email)) {
-      setEmail("");
-    } else {
-      setIsEmailValid(false);
-    }
+    window.location.href = "mailto:ramon1234rxm@gmail.com";
   };
 
   const socialLinks = [
@@ -49,32 +41,13 @@ export const Footer = () => {
 
         <div className="w-full md:w-auto md:min-w-[300px]">
           <h3 className="text-gray-500 mb-4">Stay connected w/ me.</h3>
-          <form onSubmit={handleSubmit} className="flex">
-            <div className="relative flex-grow">
-              <input
-                type="email"
-                value={email}
-                placeholder="Enter your email"
-                className={`bg-transparent border-b ${
-                  isEmailValid ? "border-gray-500" : "border-red-500"
-                } py-2 focus:outline-none focus:border-white w-full pr-10`}
-                onChange={(e) => {
-                  setEmail(e.target.value);
-                  setIsEmailValid(true);
-                }}
-                aria-label="Email address"
-              />
-              {!isEmailValid && (
-                <p className="text-red-500 text-xs mt-1">
-                  Please enter a valid email
-                </p>
-              )}
-            </div>
+          <form onSubmit={handleSubmit} className="self-end">
             <button
               type="submit"
-              className="px-3 hover:text-gray-300 transition-colors"
+              className="py-2 flex cursor-pointer transition-all items-center gap-2 delay-150 text-white rounded-lg hover:bg-gray-950"
               aria-label="Submit email"
             >
+              Send Mail
               <ArrowUpRight className="h-5 w-5" />
             </button>
           </form>
