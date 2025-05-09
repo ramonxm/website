9d857aa9176cbf3a66612f7107ff1ad3d35713d3
Ramon Xavier <ramon1234rxm@gmail.com>
2025-05-09 16:24:32 -0300

    feat(page): enhance homepage layout with new logo and background pattern
    This commit updates the homepage by adding a new logo and a background pattern for improved visual appeal. The content has been restructured to better highlight the developer's experience and skills.

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,32 @@
 import { BlogPosts } from 'app/components/posts'
+import Image from 'next/image'
 
 export default function Page() {
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        Ramon Xavier
-      </h1>
-      <p className="mb-4">
-        I am a Software Developer with extensive experience in developing applications with React Native and websites with React.
-      </p>
+        <div className="w-full flex flex-col items-center mb-16 relative overflow-hidden">
+        <div className="absolute top-0 right-0 -z-10 opacity-5 transform rotate-12">
+          <Image
+            width={400} 
+            height={400} 
+            src="/svgs/pattern.svg"
+            alt="Background pattern"
+          />
+        </div>
+        
+        <Image 
+          priority
+          height={80} 
+          width={300}
+          className="mb-8"
+          alt="Ramon Xavier Logo" 
+          src="/svgs/logo_name.svg" 
+        />
+        <p className="text-center text-neutral-700 dark:text-neutral-400 max-w-md mb-8">
+         I'm a Software Developer with extensive experience in developing applications with React Native and websites with React.
+        </p>
+
+      </div> 
       <div className="my-8">
         <BlogPosts />
       </div>
